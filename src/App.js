7709f9da2be0f1fc9308c2acc8ad1ef1964e4fa7
Ola <kurdej.aleksandra@gmail.com>
2021-01-26 20:01:26 +0100

    First polygon displayed via Leaflet

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import GlobalStyle from "./theme/GlobalStyle";
+import Map from "./components/Map";
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Map />
+    </>
+  );
+};
+
+export default App;
---
+++ src/theme/GlobalStyle.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+html{
+
+}
+
+body{
+    background-color: yellow;
+margin: 0;
+display: flex;
+height: 100vh;
+align-items: center;
+justify-content: center;
+}
+`;
+
+export default GlobalStyle;
